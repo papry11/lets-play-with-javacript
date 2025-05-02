8b0537b0307b9b8d409e72fc7137095d92ba1c9b
papry <jannatulferdouspapry115@gmail.com>
2025-05-02 14:17:20 +0600

    speech converter add

--- script.js
+++ script.js
@@ -56,8 +56,31 @@ checkWeather();
 
 // -----------------------weather app end----------------------
 
-// -----------------------start--------------------
-// -----------------------end--------------------
+// -----------------------speech-converte start------------------
+
+let speech = new SpeechSynthesisUtterance()
+
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () =>{  
+    voices = window.speechSynthesis.getVoices(); 
+    speech.voice = voices[0];
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))); 
+};
+
+
+voiceSelect.addEventListener("change" , () =>{
+
+    speech.voice = voices[voiceSelect.value] 
+
+});
+
+document.querySelector(".conv-btn").addEventListener("click", () =>{    
+    speech.text = document.querySelector("textarea").value;             
+    window.speechSynthesis.speak(speech);                                
+});
+// -----------------------speech-converte end--------------------
 
 
 // -----------------------start--------------------
